a04eefb2f07cc0e8b30af9a9ca70cbeb54d5d3ad
Syntvx Bytez <syntvxbytez@gmail.com>
2025-07-31 13:53:28 -0400

    Build Corrections

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/fabianvwebsite/", // 👈 set this to match your GitHub repo
   plugins: [tailwindcss()],
 });
+
