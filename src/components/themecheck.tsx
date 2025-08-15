fc94327c3e1ffe7fea85282a5cfd35550d68a606
gitforfabianv <emailforfabian.v@gmail.com>
2025-08-15 14:57:16 -0400

    Updates

--- src/components/themecheck.tsx
+++
@@ -1,5 +0,0 @@
-export const ThemeCheck = () => (
-  <div className="h-50 w-50 bg-[#4caf50] text-white font-pixel p-4 shadow-win border-4 border-winDark">
-    Tailwind theme is working
-  </div>
-);
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  base: "/fabianvwebsite/", // GitHub repo
-  plugins: [tailwindcss()],
-});
